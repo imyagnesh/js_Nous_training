a75b5637b8ce93b7d532a55e669cb206422d95f9
Yagnesh Modh <yagnesh.modh@gmail.com>
2021-08-10 20:42:40 +0530

    commit code

---
+++ Child.js
@@ -0,0 +1,14 @@
+const Child = () => {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti magni
+        dolorum, perferendis saepe quo totam quod corporis hic. Reiciendis
+        dolorem voluptatem alias modi dolores. Illum a nesciunt vel vero
+        consequuntur.
+      </p>
+    </>
+  );
+};
+
+export default Child;
