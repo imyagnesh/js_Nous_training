3fd6b8b903802a1e4d5e6425edde117d04274c59
Sameer <szinzuwadia@1800flowers.com>
2021-08-11 19:17:45 +0530

    CSS changes
    CSS changes

--- WeatherApp.js
+++ WeatherApp.js
@@ -1,43 +1,48 @@
-import React, { Component, createRef } from 'react';
+import React, { Component, createRef, PureComponent } from 'react';
 import './WeatherApp.css';
 import ErrorMsg from './Error';
 import LoadingMsg from './Loading';
 
-class WeatherApp extends Component {
+class WeatherApp extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       cityList: [
-        { id: '1', value: 'ahmedabad', cityName: 'Ahmedabad' },
+        { id: '1', value: 'ahmedabad', cityName: 'Ahmedabad', },
         { id: '2', value: 'banglore', cityName: 'Banglore' },
         { id: '3', value: 'Delhi', cityName: 'Delhi' },
       ],
+      selectedCity:'ahmedabad',
       error: null,
       isLoaded: false,
       apiResponse: [],
     };
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  
+  handleChange(e) {
+     
+    this.setState({ selectedCity: e.target.value },()=>{
+      console.log("city Selected="+this.state.selectedCity);
+    });
+     
+  }
 
   getTemperature = () => {
     console.log('getTemperature');
+    console.log("city Selected="+this.state.selectedCity);
+    this.setState({ isLoaded: true });
     fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(res => res.json())
       .then(
         result => {
           setTimeout(
-            () =>  console.log('timeout'), 
-            1000
+            function () {
+              this.setState({ isLoaded: false });
+            }.bind(this),
+            1500,
           );
-          console.log(result);
-          this.setState({
-            isLoaded: false,
-          });
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
         error => {
           this.setState({
             isLoaded: false,
@@ -48,7 +53,7 @@ class WeatherApp extends Component {
   };
 
   render() {
-    const { error, isLoaded } = this.state;
+    const { error, isLoaded,selectedCity } = this.state;
     if (error) {
       return (
         <>
@@ -65,9 +70,9 @@ class WeatherApp extends Component {
       return (
         <>
           <h1>Weather App</h1>
-          <div>
+          <div className='weather'>
             <label>Select City:</label>
-            <select>
+            <select value={this.state.selectedCity} onChange={this.handleChange} >
               {this.state.cityList.map(city => (
                 <option key={city.id} value={city.value}>
                   {city.cityName}
