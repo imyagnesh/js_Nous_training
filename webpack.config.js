36bbadea3a65ae688e039df81ddab3d91f5ff62d
Sameer <szinzuwadia@1800flowers.com>
2021-08-11 15:19:16 +0530

    basic setup
    copy project from branch

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: ["style-loader","css-loader"]
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+      inject:'body',
+    }),
+  ],
+};
